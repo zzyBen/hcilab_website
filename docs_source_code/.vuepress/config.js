c4e44832cc165b83bb36664927fc31f7756f6219
Ziyu Zhou <897224436@qq.com>
2021-04-17 00:17:51 -0400

    add the base path setting

--- docs_source_code/.vuepress/config.js
+++ docs_source_code/.vuepress/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: 'Tufts HCI Lab',
   description: 'description in config.js',
-  // base: "/hcilab_website",
-  dest: "./docs",
+  base: "/hcilab_website",      // If the website is deployed to a specific GitHub repo, set the base path to the repo name. If it's deployed to the root GitHub Page, comment this line or use:      base: "/",
+  dest: "./docs",               // Generate the website app into docs folder, for GitHub Page deployment
   themeConfig: {
     // logo: '/logo.png',
     lastUpdated: 'Last Updated ',
